7de868a5d1d8e5fd12fe2a842fa1e361398bf5bd
Vit Benton <Vit@vitbenton.com>
2025-03-03 16:57:55 -0500

    responsive Picture example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import Picture from "./components/Picture"
-import { SourceType } from "./components/Picture"
+import { SourceType, MediaQueryTypes } from "./components/Picture"
 
 function App() {
   const fallbackImage = {
     alt: 'Maine Lighthouse',
-    height: '455px',
-    width: '683px',
     loadingStrategy: 'lazy' as 'lazy',
     src: '/lighthouse-1x.jpg',
   }
 
-  const sources = [
+  const resolutionSources = [
     {
       srcset: '/lighthouse-2x.jpg 2x, /lighthouse-3x.jpg 3x',
       type: 'image/jpeg' as SourceType,
     },
   ]
 
+  const widthSources = [
+    {
+      srcset: '/lighthouse-mobile-2x.jpg 2x, /lighthouse-mobile-3x.jpg 3x',
+      media: '(max-width: 799px)' as MediaQueryTypes,
+      type: 'image/jpeg' as SourceType,
+    },
+    {
+      srcset: '/lighthouse-2x.jpg 2x, /lighthouse-3x.jpg 3x',
+      media: '(min-width: 800px)' as MediaQueryTypes,
+      type: 'image/jpeg' as SourceType,
+    },
+  ]
+
   return (
     <>
-      <Picture fallback={fallbackImage} sources={sources} />
+      <h3>An image that is loaded by device resolution:</h3>
+      <Picture fallback={fallbackImage} sources={resolutionSources} />
+
+      <h3>An image that is loaded by device screen width:</h3>
+      <Picture fallback={fallbackImage} sources={widthSources} />
     </>
   )
 }
