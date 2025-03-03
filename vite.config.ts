d6e1c43388f16c252d227fa0fc40c0063ecfcfac
Vit Benton <Vit@vitbenton.com>
2025-03-03 17:38:58 -0500

    dynamic base value

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/native-web-components",
+  base: process.env.NODE_ENV === 'production' ? '/native-web-components' : '/',
   plugins: [react()],
 })
