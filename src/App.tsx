77f6f8f3b42b5370d248d8a279ce1ce36b135ed4
Vit Benton <Vit@vitbenton.com>
2025-03-03 17:44:06 -0500

    img src correction

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,24 @@ function App() {
   const fallbackImage = {
     alt: 'Maine lighthouse during the day',
     loadingStrategy: 'lazy' as 'lazy',
-    src: '/lighthouse-1x.jpg',
+    src: './lighthouse-1x.jpg',
   }
 
   const resolutionSources = [
     {
-      srcset: '/lighthouse-2x.jpg 2x, /lighthouse-3x.jpg 3x',
+      srcset: './lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
       type: 'image/jpeg' as SourceType,
     },
   ]
 
   const widthSources = [
     {
-      srcset: '/lighthouse-mobile-2x.jpg 2x, /lighthouse-mobile-3x.jpg 3x',
+      srcset: './lighthouse-mobile-2x.jpg 2x, ./lighthouse-mobile-3x.jpg 3x',
       media: '(max-width: 799px)' as MediaQueryTypes,
       type: 'image/jpeg' as SourceType,
     },
     {
-      srcset: '/lighthouse-2x.jpg 2x, /lighthouse-3x.jpg 3x',
+      srcset: './lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
       media: '(min-width: 800px)' as MediaQueryTypes,
       type: 'image/jpeg' as SourceType,
     },
