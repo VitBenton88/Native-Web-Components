1517645db18d6294a7a275f54d3defd27c632f39
Vit Benton <Vit@vitbenton.com>
2025-03-04 07:26:59 -0500

    Dialog install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import './App.css'
+
+import Dialog from "./components/Dialog"
 import Picture from "./components/Picture"
 import { SourceType, MediaQueryTypes } from "./components/Picture"
 
 function App() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isModalDialogOpen, setIsModalDialogOpen] = useState(false);
+
   const fallbackImage = {
     alt: 'Maine lighthouse during the day',
     loadingStrategy: 'lazy' as 'lazy',
@@ -30,15 +37,40 @@ function App() {
 
   return (
     <>
-      <figure>
-        <Picture fallback={fallbackImage} sources={resolutionSources} />
-        <figcaption>An image that is loaded by device resolution 👆</figcaption>
-      </figure>
+      <h1>Native web components!</h1>
+      <article>
+        <h2>Responsive & dynamic images</h2>
+        <figure>
+          <Picture fallback={fallbackImage} sources={resolutionSources} />
+          <figcaption>An image that is loaded by device resolution 👆</figcaption>
+        </figure>
 
-      <figure>
-        <Picture fallback={fallbackImage} sources={widthSources} />
-        <figcaption>An image that is loaded by device screen width & resolution 👆</figcaption>
-      </figure>
+        <figure>
+          <Picture fallback={fallbackImage} sources={widthSources} />
+          <figcaption>An image that is loaded by device size & resolution 👆</figcaption>
+        </figure>
+      </article>
+
+      <article>
+        <h2>Dialogs and modals</h2>
+        <div>
+          <button onClick={() => setIsDialogOpen(true)}>Open Dialog</button>
+          <Dialog isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+            <h2>Dialog Content</h2>
+            <p>This is the dialog content.</p>
+            <button onClick={() => setIsDialogOpen(false)}>Close</button>
+          </Dialog>
+        </div>
+
+      <div>
+        <button onClick={() => setIsModalDialogOpen(true)}>Open Modal Dialog</button>
+        <Dialog useModal isOpen={isModalDialogOpen} onClose={() => setIsModalDialogOpen(false)}>
+          <h2>Modal Dialog Content</h2>
+          <p>This is the dialog content.</p>
+          <button onClick={() => setIsModalDialogOpen(false)}>Close</button>
+        </Dialog>
+      </div>
+      </article>
     </>
   )
 }
---
+++ src/components/Dialog.tsx
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from 'react';
+
+type DialogProps = {
+  children?: React.ReactNode;
+  isOpen: Boolean;
+  useModal?: Boolean;
+  onClose: Function;
+};
+
+const Dialog: React.FC<DialogProps> = ({ children, onClose, useModal = false, isOpen = false }: DialogProps): React.ReactNode => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialogElement = dialogRef.current;
+    if (!dialogElement) return;
+
+    if (isOpen) {
+      useModal ? dialogElement.showModal() : dialogElement.show();
+    } else {
+      dialogElement.close();
+    }
+  }, [isOpen, useModal]);
+
+  const handleClose = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  const classNames = useModal ? 'modal' : '';
+
+  return (
+    <dialog className={classNames} ref={dialogRef} onClose={handleClose}>
+      {children}
+    </dialog>
+  );
+};
+
+export default Dialog;
