2f6783209f013f7e5ca0189d3793d1c5b0bad221
Vit Benton <Vit@vitbenton.com>
2025-03-04 12:07:01 -0500

    1x added

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,19 @@ function App() {
 
   const resolutionSources = [
     {
-      srcset: './lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
+      srcset: './lighthouse-1x.jpg 1x, ./lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
       type: 'image/jpeg' as SourceType,
     },
   ]
 
   const widthSources = [
     {
-      srcset: './lighthouse-1x.jpg 1x, ./lighthouse-mobile-2x.jpg 2x, ./lighthouse-mobile-3x.jpg 3x',
+      srcset: './lighthouse-mobile-2x.jpg 2x, ./lighthouse-mobile-3x.jpg 3x',
       media: '(max-width: 799px)' as MediaQueryTypes,
       type: 'image/jpeg' as SourceType,
     },
     {
-      srcset: './lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
+      srcset: './lighthouse-1x.jpg 1x, ./lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
       media: '(min-width: 800px)' as MediaQueryTypes,
       type: 'image/jpeg' as SourceType,
     },
