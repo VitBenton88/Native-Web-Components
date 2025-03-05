6e1781fde2627d96564fd0d35ab82e44294f426d
Vit Benton <Vit@vitbenton.com>
2025-03-05 11:40:23 -0500

    lang type and prop added for code block

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
+type Language =
+  | "js"       // JavaScript
+  | "ts"       // TypeScript
+  | "html"     
+  | "css"      
+  | "json"     
+  | "sh"       // Shell/Bash
+  | "py"       // Python
+  | "java"     
+  | "cs"       // C#
+  | "cpp"      // C++
+  | "php"      
+  | "rb"       // Ruby
+  | "go"       
+  | "swift"    
+  | "kt"       // Kotlin
+  | "sql"      
+  | "yaml"     
+  | "md";      // Markdown
+
 type CodeBlockProps = {
 	code: string;
+	language: Language;
 };
 
-const CodeBlock: React.FC<CodeBlockProps> = ({ code }): React.ReactNode => {
+const CodeBlock: React.FC<CodeBlockProps> = ({ code, language }): React.ReactNode => {
 	return (
 		<pre>
-			<code>{code}</code>
+			<code className={`language-${language}`}>{code}</code>
 		</pre>
 	);
 };
