2a61a82b1793b62ab28be77860d2be6aa74228ce
Vit Benton <Vit@vitbenton.com>
2025-03-06 08:58:28 -0500

    minor test refactors

--- src/components/__tests__/CodeBlock.test.tsx
+++ src/components/__tests__/CodeBlock.test.tsx
@@ -2,19 +2,17 @@ import { render, screen } from '@testing-library/react';
 import CodeBlock, { type Language } from '../CodeBlock';
 
 describe('CodeBlock', () => {
-	const renderComponent = ({code="hello world", language="js" as Language}) => {
+	const renderComponent = ({code='hello world', language='js' as Language}) => {
 		render(<CodeBlock code={code} language={language} />);
 	};
 
 	const elements = {
-		get code() {
-			return screen.getByRole('code');
-		},
+		get code() { return screen.getByRole('code'); },
 	};
 
 	describe('renders', () => {
 		beforeEach(() => {
-			renderComponent({code: "hello world", language: "js"});
+			renderComponent({code: 'hello world', language: 'js'});
 		});
 	
 		it('should render code from code prop to visible DOM', () => {
