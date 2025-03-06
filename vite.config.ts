c739b5b7b51483349bba1fd7b4eae7c04539d2b3
Vit Benton <Vit@vitbenton.com>
2025-03-06 12:38:39 -0500

    vite config base url update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/native-web-components' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Native-Web-Components' : '/',
   plugins: [react()],
   test: {
     globals: true,
