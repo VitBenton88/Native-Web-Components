943814646fe810d8bd2129b23c616eafc96e46e2
Vit Benton <Vit@vitbenton.com>
2025-03-11 13:16:49 -0400

    meter test additions

--- src/components/__tests__/Meter.test.tsx
+++ src/components/__tests__/Meter.test.tsx
@@ -1,8 +1,10 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import Meter, { type MeterProps } from '../Meter';
 
+const mockDefaultProps: MeterProps = { value: 50, min: 0, max: 100 };
+
 describe('Meter', () => {
-	const renderComponent = (propData: MeterProps = { value: 50, min: 0, max: 100 }) =>
+	const renderComponent = (propData: MeterProps = mockDefaultProps) =>
 		render(<Meter {...propData} />);
 
 	const elements = {
@@ -18,12 +20,29 @@ describe('Meter', () => {
 				renderComponent();
 			});
 
-			it('should render meter element', () => {
+			it('should render meter element with correct attributes', () => {
 				expect(elements.meter).toBeInTheDocument();
+				expect(elements.meter).toHaveAttribute("max", mockDefaultProps.max?.toString());
+				expect(elements.meter).toHaveAttribute("min", mockDefaultProps.min?.toString());
+				expect(elements.meter).toHaveAttribute("value", mockDefaultProps.value?.toString());
 			});
 
 			it('should not render label element', () => {
 				expect(elements.label).not.toBeInTheDocument();
+			});
+		});
+
+		describe('with label', () => {
+			beforeEach(() => {
+				renderComponent({ ...mockDefaultProps, label: 'test label' });
+			});
+
+			it('should render label element', () => {
+				expect(elements.label).toBeInTheDocument();
+			});
+
+			it('should render correct label text', () => {
+				expect(elements.label).toHaveTextContent('test label');
 			});
 		});
 	});
