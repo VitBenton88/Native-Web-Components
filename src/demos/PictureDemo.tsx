0803787161540448bb7aeacf30b4ae7a3e1e19f5
Vit Benton <Vit@vitbenton.com>
2025-03-11 15:33:35 -0400

    new picture demo

--- src/demos/PictureDemo.tsx
+++ src/demos/PictureDemo.tsx
@@ -7,22 +7,20 @@ const PictureDemo: React.FC = (): React.ReactNode => {
 		src: './lighthouse-1x.jpg',
 	};
 
-	const resolutionSources = [
+	const pictureSources = [
 		{
-			srcset: './lighthouse-1x.jpg 1x, ./lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
-			type: 'image/jpeg' as SourceType,
-		},
-	];
-
-	const widthSources = [
-		{
-			srcset: './lighthouse-mobile-2x.jpg 2x, ./lighthouse-mobile-3x.jpg 3x',
-			media: '(max-width: 799px)' as MediaQueryTypes,
+			srcset: './lighthouse-mobile-1x.jpg 1x, ./lighthouse-mobile-2x.jpg 2x, ./lighthouse-mobile-3x.jpg 3x, ./lighthouse-mobile-4x.jpg 4x',
+			media: '(max-width: 767px)' as MediaQueryTypes,
 			type: 'image/jpeg' as SourceType,
 		},
 		{
-			srcset: './lighthouse-1x.jpg 1x, ./lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x',
-			media: '(min-width: 800px)' as MediaQueryTypes,
+			srcset: './lighthouse-tablet-1x.jpg 1x, ./lighthouse-tablet-2x.jpg 2x, ./lighthouse-tablet-3x.jpg 3x, ./lighthouse-tablet-4x.jpg 4x',
+			media: '(max-width: 1024px)' as MediaQueryTypes,
+			type: 'image/jpeg' as SourceType,
+		},
+		{
+			srcset: './lighthouse-1x.jpg 1x, ./lighthouse-2x.jpg 2x, ./lighthouse-3x.jpg 3x, ./lighthouse-4x.jpg 4x',
+			media: '(min-width: 1025px)' as MediaQueryTypes,
 			type: 'image/jpeg' as SourceType,
 		},
 	];
@@ -30,13 +28,9 @@ const PictureDemo: React.FC = (): React.ReactNode => {
 	return (
 		<article>
 			<h2>Responsive & dynamic images</h2>
-			<figure>
-				<Picture img={fallbackImage} sources={resolutionSources} />
-				<figcaption>An image that is loaded by device resolution 👆</figcaption>
-			</figure>
 
 			<figure>
-				<Picture img={fallbackImage} sources={widthSources} />
+				<Picture img={fallbackImage} sources={pictureSources} />
 				<figcaption>An image that is loaded by device size & resolution 👆</figcaption>
 			</figure>
 		</article>
